b1f6f39f7bc5f99da8fce5433257bd959b7a1d86
Richard Onomor <onomorrichard001@gmail.com>
2024-03-26 15:25:52 +0100

    receiving from postman

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(bodyParser.json());
 app.get("/",(req,res)=>{
   res.status(200).send("hello! know now that Jesus is Lord")
 });
-// Route to handle incoming data from NodeMCU
-// app.post('/data', (req, res) => {
-//   const sensorData = req.body.sensorValue;
-//   console.log('Received sensor data:', sensorData);
-//   res.sendStatus(200); // Send OK response
-// });
+
+// Route to handle incoming data from Postman
+app.post('/data', (req, res) => {
+  const jsonData = req.body; // Get JSON data from the request body
+  console.log('Received JSON data:', jsonData);
+  res.status(200).json({ message: 'JSON data received successfully' }); // Send response
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
